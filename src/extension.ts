de2d5809c6eda6afbb9d730925583b1719326def
Jonatan Sundqvist <jonatan.sundqvist@vimla.se>
2020-04-16 00:23:35 +0200

    Some refactoring and indentation changes. The diff is terrible

--- src/extension.ts
+++ src/extension.ts
@@ -2,133 +2,164 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as vm from 'vm';
+import * as path from 'path';
+import * as process from 'process';
+import { setFlagsFromString } from 'v8';
+
+// Scratch Pad
+// const process = new vscode.ProcessExecution(`node`, );
+// Syntax check without running
+// node --check
+
+const webpanel = () => {
+  const panel = vscode.window.createWebviewPanel('', 'Millennial Mode Content', vscode.ViewColumn.Beside, {});
+  panel.webview.html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cat Coding</title>
+  <script>
+  console.log('Hello World From the Webview')
+  </script>
+  </head>
+  <body>
+  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+  </body>
+  </html>`;
+};
+
+namespace millennial {
+  export class ExecutionSession {
+    public context: vm.Context;
+    public activeDecorations: vscode.TextEditorDecorationType[];
+    public peekedValues: { [line: number]: any[] };
+    public peekedValuesColour = `rgba(60,80,242,255)`;
+
+    constructor() {
+      this.activeDecorations = [];
+      this.peekedValues = [];
+      
+      const context: any = {};
+      
+      context.global = {};
+      context.require = (dependency: string) => require(dependency);
+      context.peek = (msg:string) => {
+        // The stack index (4) was found by trial and error. Not exactly sure what the first three frames are.
+        const line: any = vm.runInContext('__stack()[4].getLineNumber()', this.context);
+        this.peekedValues[line] = (line in this.peekedValues) ? this.peekedValues[line] : [];
+        this.peekedValues[line].push(msg);
+      };
+
+      context.setPeekedValuesColour = (colour: string) => { this.peekedValuesColour = colour; };
+
+      this.context = vm.createContext(context);
+    }
+
+    // Just a string for now. Would be interesting to have interactive structures at some point.
+    public renderPeekedValues(values: any): string {
+      const list = values.map((e:any) => e.toString()).join(', ');
+      return `<- ${list}`;
+    }
+
+    dispose() {
+      this.activeDecorations.forEach(decoration => decoration.dispose()); // Clear previous results
+    }
+    
+  }
+      
+  // Code for finding the source line of a function invokation.
+  // By running it in the same context as the client script, we effectively inject
+  // these definitions into it without having to pollute it by - say - prepending this code snippet directly,
+  // which would affect the line numbers in a confusing way.
+  export const injectedDefinitionsSource: string = `
+    function __stack() {
+      let orig = Error.prepareStackTrace;
+      Error.prepareStackTrace = function(_, stack){ return stack; };
+      let err = new Error;
+      Error.captureStackTrace(err, arguments.callee);
+      let stack = err.stack;
+      Error.prepareStackTrace = orig;
+      return stack;
+    }
+    
+    Object.defineProperty(global, '___line', {
+      get: () => global['__stack'][1].getLineNumber()
+    });
+  `;
+
+  export const injectedDefinitionsCompiledScript: vm.Script = new vm.Script(millennial.injectedDefinitionsSource);
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "millennial-mode-extension" is now active!');
+  
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  
+  const outputChannel = vscode.window.createOutputChannel('Millennial Mode');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "millennial-mode-extension" is now active!');
+  let previousSession: millennial.ExecutionSession | null = null;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	context.subscriptions.push(vscode.commands.registerCommand('millennial-mode-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  context.subscriptions.push(vscode.commands.registerCommand('millennial-mode-extension.millennialMode', async () => {
+    const root = vscode.workspace?.workspaceFolders?.[0].uri.fsPath;
+    const nameOfVirtualFile = path.join(root || '.', 'millennial.js');
+    const document = await vscode.workspace.openTextDocument(vscode.Uri.parse(`untitled:${nameOfVirtualFile}`, true));
+    const editor = await vscode.window.showTextDocument(document);
+    outputChannel.show();
+      
+    vscode.window.onDidChangeTextEditorSelection(event => {
+      if (event.textEditor === editor) {
+        // TextEditorSelectionChangeKind
+      }
+    });
+    
+    vscode.workspace.onDidChangeTextDocument(event => {
+      if (event.document !== document) { return; }
+      
+      if (previousSession !== null) {
+        previousSession.dispose();
+      }
+      
+      const session = new millennial.ExecutionSession();
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('OMG WHERE IS MY CODE. OH. FOUND IT.');
-	}));
+      try {
+        process.chdir(root || '.');
+        millennial.injectedDefinitionsCompiledScript.runInContext(session.context, {});
+        const result = vm.runInContext(document.getText(), session.context, { filename: nameOfVirtualFile });
 
-	// let anotherDisposable = vscode.window.activeTextEditor?.setDecorations
-	// node_modules/@types/vscode/index.d.ts
-	// export const onDidChangeActiveTextEditor: Event<TextEditor | undefined>;
-	const outputChannel = vscode.window.createOutputChannel('Millennial Mode');
-
-	const webpanel = () => {
-		const panel = vscode.window.createWebviewPanel('', 'Millennial Mode Content', vscode.ViewColumn.Beside, {});
-		panel.webview.html = `<!DOCTYPE html>
-		<html lang="en">
-		<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Cat Coding</title>
-		<script>
-		console.log('Hello World From the Webview')
-		</script>
-		</head>
-		<body>
-		<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
-		</body>
-		</html>`;
-	};
-
-	context.subscriptions.push(vscode.commands.registerCommand('millennial-mode-extension.millennialMode', async () => {
-		// vscode.workspace.onDidChangeTextDocument
-		vscode.window.showInformationMessage('OH NO ');
-		// vscode.window.showTextDocument(, {})
-		const doc = await vscode.workspace.openTextDocument(vscode.Uri.parse(`untitled:millennial.js`, true));
-		// const document = await vscode.workspace.openTextDocument({ language: 'js', content: '// Write your millennial code here' });
-		// vscode.window.showInformationMessage(document.fileName);
-		const editor = await vscode.window.showTextDocument(doc);
-		
-		outputChannel.show();
-
-		// const process = new vscode.ProcessExecution(`node`, );
-
-		// Syntax check without running
-		// node --check
-
-		const preface = `
-			function __stack() {
-				var orig = Error.prepareStackTrace;
-				Error.prepareStackTrace = function(_, stack){ return stack; };
-				var err = new Error;
-				Error.captureStackTrace(err, arguments.callee);
-				var stack = err.stack;
-				Error.prepareStackTrace = orig;
-				return stack;
-			}
-				
-			Object.defineProperty(global, '___line', {
-				get: function(){
-					return global['__stack'][1].getLineNumber();
-				}
-			});
-		`;
-
-		const defineStack = new vm.Script(preface);
-
-		let context: any = {};
-
-		const printedExpressions: { [line:number]: any } = {};
-
-		vscode.workspace.onDidChangeTextDocument(event => {
-			if (event.document === doc) {
-				context = {
-					x: 5,
-					global: {},
-					log: (msg:string) => {
-						// vscode.window.showInformationMessage(msg);
-						const metadata: any = vm.runInContext('__stack()[4].getLineNumber()', context);
-						printedExpressions[metadata] = msg;
-
-						// outputChannel.append(metadata);
-						// outputChannel.appendLine(msg);
-						// outputChannel.appendLine(`[${__stack()}] ${msg}`);
-						// vscode.window.showInformationMessage(`[${__stack()}] ${msg}`);
-					}
-				};
-				try {
-					context = vm.createContext(context);
-					defineStack.runInContext(context, {});
-					const result = vm.runInContext(doc.getText(), context, { filename: 'millennial' });
-
-					// Show in-line results
-					if (editor) {
-						Object.entries(printedExpressions).forEach(([line, expression]) => {
-							const decorationType = vscode.window.createTextEditorDecorationType({
-								backgroundColor: 'rgba(1,1,1,0)',
-								after: { 'contentText': String(expression), borderColor: 'white', margin: '30px' }
-							});
-							editor.setDecorations(decorationType, [editor.document.lineAt(parseInt(line) - 1).range]);
-						});
-
-					}
-
-					vscode.window.showInformationMessage(result);
-				} catch(error) {
-					console.log(error);
-				}
-			}
-		});
-		// onDidChangeTextEditorSelection
-		
-	}));
-
-	// context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor((meditor) => {}))
+        // Show in-line results
+        Object.entries(session.peekedValues).forEach(([line, expressions]) => {
+          const decorationType = vscode.window.createTextEditorDecorationType({
+            after: {
+              // TODO: Display type information
+              color: session.peekedValuesColour,
+              fontWeight: 'bold',
+              contentText: session.renderPeekedValues(expressions),
+              borderColor: 'white',
+              margin: '12px'
+            }
+          });
+          session.activeDecorations.push(decorationType);
+          editor.setDecorations(decorationType, [editor.document.lineAt(parseInt(line) - 1).range]);
+        });
+        vscode.window.showInformationMessage(result);
+        previousSession = session;
+      } catch(error) {
+        outputChannel.appendLine(error);
+        console.error(error);
+        session.dispose();
+      }
+    });
+      
+  }));
 }
-
+  
 // this method is called when your extension is deactivated
 export function deactivate() {}
